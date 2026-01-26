73c64d6552233b464b71cdb381a0c2b9c89a8f1f
weididev <snehamoydutta11@gmail.com>
2026-01-26 17:33:20 +0530

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,18 @@
+// Central Configuration File
+// Update all settings here
+
+export const APP_CONFIG = {
+  version: '1.4.0', // Bumped version for release
+  ads: {
+    // Set this to TRUE when you are ready to publish with real ads
+    enableRealAds: true, 
+    
+    // Paste your Real AdMob IDs here
+    realAppId: 'ca-app-pub-2018579775913448~9911402503', 
+    realBannerId: 'ca-app-pub-2018579775913448/8322988663',
+    
+    // Google's Test IDs (Do not change these)
+    testAppId: 'ca-app-pub-3940256099942544~3347511713',
+    testBannerId: 'ca-app-pub-3940256099942544/6300978111',
+  }
+};
